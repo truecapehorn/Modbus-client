c220ae1bb64aa234ee065b36e3fdf3f1e3e73ea9
truecapehorn <truecapehorn@gmail.com>
2019-09-14 16:31:32 +0200

    rozdzial odczytu rejestrow i cewek na dwie klasy

--- client.py
+++ client.py
@@ -7,12 +7,12 @@ class Klient:
         self.path = path
         self.client_type = client_type
 
-    def open_config(self):
+    def _open_config(self):
         with open(self.path) as json_file:
             return json.load(json_file)
 
     def get_parm(self):
-        _config = self.open_config()
+        _config = self._open_config()
         _dict_parm = _config['client'][self.client_type]
         return _dict_parm
 
@@ -30,19 +30,38 @@ class Klient:
 # ===================================================================================================================
 
 class Odczyt:
-    def __init__(self, client, unit, reg_start, reg_lenght, data_type, transp=True):
+    def __init__(self, client, unit, reg_start, reg_lenght, **kwargs):
         self.client = client
         self.unit = unit
-        self._reg_start = reg_start
-        self._reg_lenght = reg_lenght
+        self.reg_start = reg_start
+        self.reg_lenght = reg_lenght
+
+    def reg_adress(self):
+        return self.reg_start
+
+    def set_reg_adress(self, adress):
+        self.reg_start = adress
+
+    def lenght_data(self):
+        return self.reg_lenght
+
+    def set_lenght_data(self, size):
+        self.reg_lenght = size
+
+
+# ====================================================================================================================
+
+class Rejestry(Odczyt):
+
+    def __init__(self, client, unit, reg_start, reg_lenght, data_type, transp=True):
+        super().__init__(client, unit, reg_start, reg_lenght)
         self.data_type = data_type
         self.transp = transp
 
     def _read_reg(self):
-        reg = self.client.read_register(self.unit, self._reg_start,
-                                        self._reg_lenght, self.reg_type,
-                                        self.data_type, self.transp)
-        return reg
+        return self.client.read_register(self.unit, self.reg_start,
+                                         self.reg_lenght, self.reg_type,
+                                         self.data_type, self.transp)
 
     def read_holding(self):
         self.reg_type = 'holding'
@@ -52,20 +71,20 @@ class Odczyt:
         self.reg_type = 'input'
         return self._read_reg()
 
+
+# ===================================================================================================================
+
+class Cewki(Odczyt):
+
+    def __init__(self, client, unit, reg_start, reg_lenght):
+        super().__init__(client, unit, reg_start, reg_lenght)
+
     def _read_boll(self):
-        coil = self.client.read_bool(self.unit, self._reg_start, self._reg_lenght, self.reg_type)
-        return coil
+        return self.client.read_bool(self.unit, self.reg_start, self.reg_lenght)
 
     def read_coil(self):
         self.reg_type = 'coil'
         return self._read_boll()
-
-
-    def reg_adress(self):
-        return self._reg_start
-
-    def set_reg_adress(self, adress):
-        self._reg_start = adress
 
 
 # ====================================================================================================================
@@ -79,20 +98,22 @@ if __name__ == '__main__':
     # reg_for_check = [30201, 30233, 30531, 30775, 30795, 30803, 30805, 30813, 30837, 30839, 30769, 30771, 30773,
     #                  30957, 30959, 30961, 30537, 30953, 40212, 40915]
 
+    reg = Rejestry(client, unit=1, reg_start=1000, reg_lenght=16, data_type='int')
+    bol = Cewki(client, unit=1, reg_start=0, reg_lenght=250)
+
     reg_for_check = [i for i in range(1000, 1016)]
     nr = 1
-
-    reg = Odczyt(client, unit=3, reg_start=0, reg_lenght=1, data_type='int')
-    # bool = Odczyt(client,unit=3,reg_start=0,reg_lenght=250,)
-
     try:
         while True:
-            print(nr, end=': ')
-            for i in reg_for_check:
-                reg.set_reg_adress(i)
-                holding = reg.read_holding()
-                print(holding['Data'], end=';')
-            print('\n')
+            print(nr, end=':\t ')
+            # reg.set_reg_adress(0)
+            reg.set_lenght_data(nr)
+            bol.set_lenght_data(nr)
+            holding = reg.read_holding()
+            print(holding['Data'], end=';')
+            col = bol.read_coil()
+            print('\n\t', col['Data'])
             nr += 1
+            time.sleep(1)
     except KeyboardInterrupt:
         print('\nKoniec')
