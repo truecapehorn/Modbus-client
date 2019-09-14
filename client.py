6f766243da7ca6fb395618d9b48f9e80a2342362
truecapehorn <truecapehorn@gmail.com>
2019-09-14 13:42:16 +0200

    przed rozdizleniem klas z odczytem holding i bool

--- client.py
+++ client.py
@@ -1,13 +1,11 @@
-from Modbus_API.modbus_master import TCP_Client,RTU_Client,Master
+from Modbus_API.modbus_master import TCP_Client, RTU_Client, Master
 import json, time
 
 
-
-
-class Client:
-    def __init__(self,path,client_type):
-        self.path= path
-        self.client_type=client_type
+class Klient:
+    def __init__(self, path, client_type):
+        self.path = path
+        self.client_type = client_type
 
     def open_config(self):
         with open(self.path) as json_file:
@@ -18,34 +16,50 @@ class Client:
         _dict_parm = _config['client'][self.client_type]
         return _dict_parm
 
-
     def make_client(self):
-        _parm=self.get_parm().values()
-        if self.client_type=='tcp':
+        _master = None
+        _parm = self.get_parm().values()
+        if self.client_type == 'tcp':
             _master = TCP_Client(*_parm)
-        if self.client_type=='rtu':
+        if self.client_type == 'rtu':
             _master = RTU_Client(*_parm)
         _client = _master.client
-        return Master(_client)
+        return Master(_client)  # wywolanie klasy Master w API
+
+
+# ===================================================================================================================
 
 class Odczyt:
     def __init__(self, client, unit, reg_start, reg_lenght, data_type, transp=True):
         self.client = client
         self.unit = unit
         self._reg_start = reg_start
-        self.reg_lenght = reg_lenght
+        self._reg_lenght = reg_lenght
         self.data_type = data_type
         self.transp = transp
 
     def _read_reg(self):
         reg = self.client.read_register(self.unit, self._reg_start,
-                                   self.reg_lenght, self.reg_type,
-                                   self.data_type, self.transp)
+                                        self._reg_lenght, self.reg_type,
+                                        self.data_type, self.transp)
         return reg
 
     def read_holding(self):
-        self.reg_type='holding'
+        self.reg_type = 'holding'
         return self._read_reg()
+
+    def read_input(self):
+        self.reg_type = 'input'
+        return self._read_reg()
+
+    def _read_boll(self):
+        coil = self.client.read_bool(self.unit, self._reg_start, self._reg_lenght, self.reg_type)
+        return coil
+
+    def read_coil(self):
+        self.reg_type = 'coil'
+        return self._read_boll()
+
 
     def reg_adress(self):
         return self._reg_start
@@ -54,12 +68,12 @@ class Odczyt:
         self._reg_start = adress
 
 
+# ====================================================================================================================
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
-
-    modbus = Client('config.json','tcp')
-    client=modbus.make_client()
+    modbus = Klient('config.json', 'tcp')
+    client = modbus.make_client()
     print(modbus.get_parm())
 
     # reg_for_check = [30201, 30233, 30531, 30775, 30795, 30803, 30805, 30813, 30837, 30839, 30769, 30771, 30773,
@@ -67,16 +81,18 @@ if __name__=='__main__':
 
     reg_for_check = [i for i in range(1000, 1016)]
     nr = 1
-    reg32 = Odczyt(client, unit=3, reg_start=0, reg_lenght=1, data_type='int', transp=True)
+
+    reg = Odczyt(client, unit=3, reg_start=0, reg_lenght=1, data_type='int')
+    # bool = Odczyt(client,unit=3,reg_start=0,reg_lenght=250,)
 
     try:
         while True:
             print(nr, end=': ')
             for i in reg_for_check:
-                reg32.set_reg_adress(i)
-                holding = reg32.read_holding()
-                print(holding['Data'], end=',')
+                reg.set_reg_adress(i)
+                holding = reg.read_holding()
+                print(holding['Data'], end=';')
             print('\n')
             nr += 1
     except KeyboardInterrupt:
-        print('Koniec')
+        print('\nKoniec')
