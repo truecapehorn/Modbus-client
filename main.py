4b48204d1e1b2a10606f64058d45974fd6e0c1cc
truecapehorn <truecapehorn@gmail.com>
2019-09-14 16:51:08 +0200

    dodanie modulu mian

---
+++ main.py
@@ -0,0 +1,29 @@
+from client import Klient,Rejestry,Cewki
+import time
+
+modbus = Klient('config.json', 'tcp')
+client = modbus.make_client()
+print(modbus.get_parm())
+
+# reg_for_check = [30201, 30233, 30531, 30775, 30795, 30803, 30805, 30813, 30837, 30839, 30769, 30771, 30773,
+#                  30957, 30959, 30961, 30537, 30953, 40212, 40915]
+
+reg = Rejestry(client, unit=1, reg_start=1000, reg_lenght=16, data_type='int')
+bol = Cewki(client, unit=1, reg_start=0, reg_lenght=250)
+
+reg_for_check = [i for i in range(1000, 1016)]
+nr = 1
+try:
+    while True:
+        print(nr, end=':\t ')
+        # reg.set_reg_adress(0)
+        reg.set_lenght_data(nr)
+        bol.set_lenght_data(nr)
+        holding = reg.read_holding()
+        print(holding['Data'], end=';')
+        col = bol.read_coil()
+        print('\n\t', col['Data'])
+        nr += 1
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('\nKoniec')
