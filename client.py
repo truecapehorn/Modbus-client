2e89ffcdf02822715a3bb2e80e162ea29e8cdc73
truecapehorn <truecapehorn@gmail.com>
2019-10-08 08:57:36 +0200

    przystosowanie do rtu

--- client.py
+++ client.py
@@ -91,15 +91,14 @@ class Cewki(Odczyt):
 
 if __name__ == '__main__':
 
-    modbus = Klient('config.json', 'tcp')
+    modbus = Klient('config.json', 'rtu')
     client = modbus.make_client()
     print(modbus.get_parm())
 
     # reg_for_check = [30201, 30233, 30531, 30775, 30795, 30803, 30805, 30813, 30837, 30839, 30769, 30771, 30773,
     #                  30957, 30959, 30961, 30537, 30953, 40212, 40915]
 
-    reg = Rejestry(client, unit=1, reg_start=1000, reg_lenght=16, data_type='int')
-    bol = Cewki(client, unit=1, reg_start=0, reg_lenght=250)
+    reg = Rejestry(client, unit=64, reg_start=0, reg_lenght=2, data_type='int',)
 
     reg_for_check = [i for i in range(1000, 1016)]
     nr = 1
@@ -108,18 +107,11 @@ if __name__ == '__main__':
     def print_data(time, data):
         return print("{} : {}".format(time, data), end=';\n')
 
-
     try:
         while True:
-            # reg.set_reg_adress(0)
-            # reg.set_lenght_data(nr)
-            # bol.set_lenght_data(nr)
+
             holding = reg.read_holding()
-            time.sleep(1)
-            col = bol.read_coil()
-            print_data(holding['Time'][1], holding['Data'])
-            print_data(col['Time'][1], col['Data'])
-            nr += 1
-            time.sleep(1)
+            print_data(holding['Time'][1],holding['Data'])
+            time.sleep(2)
     except KeyboardInterrupt:
-        print('\nKoniec')
+        print_data('Stop')
