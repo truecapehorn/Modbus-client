cc176b579052986a99106e6cea52726ca03c7a16
truecapehorn <truecapehorn@gmail.com>
2019-10-15 09:44:46 +0200

    uruchominie na ADA

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 from client import Klient, Rejestry, Cewki
 import time
 
-modbus = Klient('config.json', 'tcp')
-client = modbus.make_client()
-print(modbus.get_parm())
-
-# reg_for_check = [30201, 30233, 30531, 30775, 30795, 30803, 30805, 30813, 30837, 30839, 30769, 30771, 30773,
-#                  30957, 30959, 30961, 30537, 30953, 40212, 40915]
-
-reg = Rejestry(client, unit=1, reg_start=1000, reg_lenght=16, data_type='int')
-bol = Cewki(client, unit=1, reg_start=0, reg_lenght=250)
-
-reg_for_check = [i for i in range(1000, 1016)]
-nr = 1
+modbus_rtu = Klient('config.json', 'rtu')
+client_rtu = modbus_rtu.make_client()
+print(modbus_rtu.get_parm())
 
 
-def print_data(time, data):
-    return print("{} : {}".format(time, data), end=';\n')
+reg_1 = Rejestry(client_rtu, unit=64, reg_start=0, reg_lenght=10, data_type='int')
+reg_2 = Rejestry(client_rtu, unit=25, reg_start=70, reg_lenght=10, data_type='int')
+
+
+def print_data(device ,time, data):
+    return print("Urzadzenie:{} - {} : {}".format(device, time, data), end=';\n')
 
 
 try:
     while True:
-        # reg.set_reg_adress(0)
-        # reg.set_lenght_data(nr)
-        # bol.set_lenght_data(nr)
-        holding = reg.read_holding()
-        col = bol.read_coil()
-        print_data(holding['Time'][1], holding['Data'])
-        print_data(col['Time'][1], col['Data'])
-        nr += 1
+
+        holding_1 = reg_1.read_holding()
+        print_data(holding_1['Device'],holding_1['Time'][1], holding_1['Data'])
+
+
         time.sleep(1)
 except KeyboardInterrupt:
     print('\nKoniec')
